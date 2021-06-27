5a569de5d4da4b6d5144cf66a993104e5659ba22
Deepak Mali <mail2deepakmali@gmail.com>
2021-06-27 20:07:29 +0530

    add table component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useMemo } from "react";
+import Table from "./components/Table";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
+const client = new W3CWebSocket('ws://city-ws.herokuapp.com/');
+const cityMap = {};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([]);
+
+	// Using useEffect to call the API once mounted and set the data
+	useEffect(() => {
+		(async () => {
+			client.onopen = () => {
+				console.log('WebSocket Client Connected');
+			};
+			client.onmessage = (message) => {
+				message = JSON.parse(message.data);
+				setData([data, ...message]);
+				message.forEach((d) => {
+					if (!cityMap[d.city]) {
+						cityMap[d.city] = d;
+						setData([...data, d]);
+					} else {
+						cityMap[d.city].aqi = d.aqi;
+					}
+				});
+			};
+		})();
+	}, [data]);
+
+
+	const columns = useMemo(
+		() => [{
+			Header: 'City',
+			accessor: 'city'
+		}, {
+			Header: 'Current AQI',
+			accessor: 'aqi'
+		}],
+		[]
+	);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Table columns={columns} data={data} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
